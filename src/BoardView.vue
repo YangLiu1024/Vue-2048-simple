60173a557c8bc88c705cb817b0b7aee3a08c667f
YangLiu1024 <31921184+YangLiu1024@users.noreply.github.com>
2020-01-09 16:04:12 +0800

    Add files via upload

---
+++ src/BoardView.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="board">
+        <div v-for="(row, index) in board.tiles" :key="index">
+            <tile v-for="t in row" :key="t.id" :tile="t"></tile>
+        </div>
+        <game-end-view @reset="onReset" :board="board"></game-end-view>
+    </div>
+</template>
+
+<script>
+import Tile from './Tile'
+import {Board} from './board'
+import GameEndView from './GameEndView'
+
+export default {
+    data() {
+        return {
+            board: new Board()
+        }
+    },
+    mounted(){
+        window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    },
+    beforeDestroy(){
+        window.removeEventListener('keydown', this.handleKeyDown.bind(this));
+    },
+    methods: {
+        handleKeyDown(event) {
+            if (this.board.won) {
+                return;
+            }
+            if (event.keyCode >=37 && event.keyCode <= 40) {
+                event.preventDefault()
+                this.board.move(event.keyCode - 37)
+            }
+        },
+        onReset() {
+            this.board = new Board()
+        }
+    },
+    components: {
+        Tile,
+        GameEndView
+    }
+}
+</script>
+<style scoped>
+.board {
+  width: 440px;
+  height: 440px;
+  padding: 5px;
+  background-color: #baa;
+  border-radius: 7px;
+  outline: none;
+  position: relative;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import BoardView from './BoardView.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(BoardView)
+})
